0e125091c876fbd351fac3824cc2308127b30fc4
Soeren <Soeren@DESKTOP-ATV5UHH>
2016-04-12 16:11:18 +0200

    final sheet

---
+++ SecondSheet/src/test/Test_4.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import _secondGiven.FileReader;
+import _secondWork.PositionalIndex;
+
+// This tests if a search for a phrase with 3 terms returns the correct document-numbers
+
+public class Test_4 {
+
+	// Location of test collection
+	private static final String TEST_PATH_3 = "collections/testCollections/third";
+
+	// Variable containing an instance of PositionalIndex
+	private PositionalIndex positionalIndex;
+
+	@BeforeClass
+	public void setupBeforeClass() throws FileNotFoundException {
+		positionalIndex = new PositionalIndex(FileReader.readCollection(TEST_PATH_3));
+	}
+
+	@Test
+	public void TestThreeTerms() {
+		ArrayList<Integer> arrayList = positionalIndex.searchForPhrase(new String[]{"his", "soul", "was"});
+		Collections.sort(arrayList);
+		Integer[] array = arrayList.toArray(new Integer[arrayList.size()]);
+		assertArrayEquals(new Integer[] { 1 }, array);
+	}
+
+	@Test
+	public void TestThreeTermsLastOneMissing() {
+		ArrayList<Integer> arrayList = positionalIndex.searchForPhrase(new String[]{"psychiatrist", "physicist", "marcus"});
+		Collections.sort(arrayList);
+		Integer[] array = arrayList.toArray(new Integer[arrayList.size()]);
+		assertArrayEquals(new Integer[] { }, array);
+	}
+}
